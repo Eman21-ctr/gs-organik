42f4fc49e362260d9d36834de42493e98c3ce1af
Eman <journalwarga@gmail.com>
2025-09-16 18:57:51 +0800

    feat: replace phone number

--- src/constants.js
+++ src/constants.js
@@ -1,5 +1,5 @@
 
-export const WHATSAPP_NUMBER = "6281353087786"; // Ganti dengan nomor Admin GS Organik
+export const WHATSAPP_NUMBER = "6282145799141"; // Ganti dengan nomor Admin GS Organik
 export const COMPANY_NAME = "GS Organik";
 export const ADMIN_EMAIL = "journalwarga@gmail.com";
 export const ADMIN_PASSWORD = "password123";
